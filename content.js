ef14521937526a4467d4ce71989b451009689522
montedev0516 <montecristodev0516@gmail.com>
2024-09-05 11:57:02 -0400

    update

--- content.js
+++ content.js
@@ -1,10 +1,30 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'start') {
         const nameElement = document.querySelector("h1.text-heading-xlarge.inline.t-24.v-align-middle.break-words");
-        // const button = document.querySelector("#changeColorBtn");
+        const sections = document.querySelectorAll("section.artdeco-card");
+        
+        const res = new Array();
+        for (const section of sections) {
+            const isExperience = section.querySelector("#experience");
+            if (isExperience) {
+                const experiences = section.querySelectorAll("li.artdeco-list__item");
+                for (const experience of experiences) {
+                    const mainBox = experience.querySelector("div > div.display-flex.flex-column.full-width.align-self-center > div.display-flex.flex-row.justify-space-between");
 
-        const name = nameElement.textContent;
+                    if (mainBox) {
+                        res.push(mainBox.innerHTML);
+                    }
 
-        sendResponse({ data: name });
+
+                }
+
+                
+            }
+        }
+
+        sendResponse({ data: res });
+        // const name = nameElement.textContent;
+
+        
     }
 });
