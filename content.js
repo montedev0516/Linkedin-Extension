679e7bbe8462f88a7b61ef7d2874bdc818291716
montedev0516 <montecristodev0516@gmail.com>
2024-09-06 02:15:53 +0100

    update 2

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'start') {
         const nameElement = document.querySelector("h1.text-heading-xlarge.inline.t-24.v-align-middle.break-words");
         const sections = document.querySelectorAll("section.artdeco-card");
-        
+
         const res = new Array();
         for (const section of sections) {
             const isExperience = section.querySelector("#experience");
@@ -12,19 +12,18 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
                     const mainBox = experience.querySelector("div > div.display-flex.flex-column.full-width.align-self-center > div.display-flex.flex-row.justify-space-between");
 
                     if (mainBox) {
-                        res.push(mainBox.innerHTML);
+                        const spans = mainBox.querySelectorAll('span[aria-hidden="true"]');
+                        for (const span in spans) {
+                            res.push(span.textContent);
+                        }
                     }
-
-
                 }
-
-                
             }
         }
 
         sendResponse({ data: res });
         // const name = nameElement.textContent;
 
-        
+
     }
 });
